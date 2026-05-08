62324887523d50eb4eba409baca8dbd635fe9dd0
ngoohung <hungngoquangg@gmail.com>
2026-05-08 10:02:27 +0700

    Copy all code gen by Claude, but error don't run

--- lib/core/tokens/app_radius.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppRadius {
-  AppRadius._();
-  static const badge  = BorderRadius.all(Radius.circular(4));
-  static const button = BorderRadius.all(Radius.circular(6));
-  static const card   = BorderRadius.all(Radius.circular(8));
-  static const avatar = BorderRadius.all(Radius.circular(9999));
-}
